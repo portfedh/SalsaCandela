c69004ab41eed4a84cde8d3a5d89d2dbbfd787b4
Portfedh <59422723+portfedh@users.noreply.github.com>
2024-01-04 22:53:07 -0600

    Improve discount text

--- public/js/countdownTimer.js
+++ public/js/countdownTimer.js
@@ -4,7 +4,8 @@
   const hour = minute * 60;
   const day = hour * 24;
 
-  nextCourse = "01/10/2024";
+  let nextCourse = "01/02/2024";
+  let endMessage = "Nuestra Preventa Termino.";
 
   const countDown = new Date(nextCourse).getTime(),
     x = setInterval(function () {
@@ -22,13 +23,50 @@
           (distance % minute) / second
         ));
 
-      //do something later when date is reached
+      // Action when date is reached
       if (distance < 0) {
-        document.getElementById("headline").innerText = "It's my birthday!";
-        document.getElementById("countdown").style.display = "none";
-        document.getElementById("content").style.display = "block";
+        document.getElementById("countdown-div").style.display = "none";
         clearInterval(x);
       }
-      //seconds
+      // Seconds
     }, 0);
 })();
+
+// Original code before big changes:
+
+// (function () {
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
+
+//   let nextCourse = "01/02/2024";
+//   let endMessage = "Nuestra Preventa Termino.";
+
+//   const countDown = new Date(nextCourse).getTime(),
+//     x = setInterval(function () {
+//       const now = new Date().getTime(),
+//         distance = countDown - now;
+
+//       (document.getElementById("days").innerText = Math.floor(distance / day)),
+//         (document.getElementById("hours").innerText = Math.floor(
+//           (distance % day) / hour
+//         )),
+//         (document.getElementById("minutes").innerText = Math.floor(
+//           (distance % hour) / minute
+//         )),
+//         (document.getElementById("seconds").innerText = Math.floor(
+//           (distance % minute) / second
+//         ));
+
+//       // Action when date is reached
+//       if (distance < 0) {
+//         document.getElementById("index-countdown-headline").innerText =
+//           endMessage;
+//         document.getElementById("countdown").style.display = "none";
+//         document.getElementById("content").style.display = "block";
+//         clearInterval(x);
+//       }
+//       // Seconds
+//     }, 0);
+// })();
