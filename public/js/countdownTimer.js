0932fae4da993252c18a2dbe762cfb56872f9162
Portfedh <59422723+portfedh@users.noreply.github.com>
2024-01-05 13:31:17 -0600

    Added Discounts to timer

--- public/js/countdownTimer.js
+++ public/js/countdownTimer.js
@@ -1,13 +1,12 @@
 (function () {
+  let nextCourseDate = "01/06/2024";
+
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  let nextCourse = "01/02/2024";
-  let endMessage = "Nuestra Preventa Termino.";
-
-  const countDown = new Date(nextCourse).getTime(),
+  const countDown = new Date(nextCourseDate).getTime(),
     x = setInterval(function () {
       const now = new Date().getTime(),
         distance = countDown - now;
@@ -23,9 +22,30 @@
           (distance % minute) / second
         ));
 
-      // Action when date is reached
+      // Action when date is reached. Eliminate discount.
       if (distance < 0) {
         document.getElementById("countdown-div").style.display = "none";
+
+        document.getElementById(
+          "descuento-niv-1-individual-porcentaje"
+        ).style.display = "none";
+
+        document.getElementById(
+          "descuento-niv-1-pareja-porcentaje"
+        ).style.display = "none";
+
+        document.getElementById(
+          "precio-original-niv-1-individual"
+        ).style.display = "none";
+
+        document.getElementById("precio-original-niv-1-pareja").style.display =
+          "none";
+
+        document.getElementById("precio-niv-1-individual").innerText =
+          "$ 1,300";
+
+        document.getElementById("precio-niv-1-pareja").innerText = "$ 2,000";
+
         clearInterval(x);
       }
       // Seconds
