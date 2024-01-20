40f2ec88db3baf60ae027c06166bf222b20960a1
Portfedh <59422723+portfedh@users.noreply.github.com>
2024-01-19 18:23:16 -0600

    Connect to mongoose before listening to ports

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const result = require("dotenv").config({ path: "./config/.env" });
 // Database
 // ========
 const connectDB = require("./config/database");
-connectDB();
 
 // Stripe
 // ======
@@ -79,6 +78,10 @@ app.use("/report", reportRoutes);
 
 // Server Port
 // ===========
-app.listen(process.env.PORT || PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
-});
+// Connect to Mongoose before listening to ports
+// So serverless applications always work
+connectDB().then(() => {
+  app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+  });
+})
