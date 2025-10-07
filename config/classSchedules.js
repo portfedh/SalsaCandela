e286610eab7d64687f670918896fbcacc020067c
Pablo Cruz Lemini <portfedh@gmail.com>
2025-10-06 20:56:07 -0600

    Add enrollment URLs and enhance schedule display for salsa and bachata classes

--- config/classSchedules.js
+++ config/classSchedules.js
@@ -9,16 +9,56 @@ const branches = [
     googleMapsEmbedUrl:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3763.5078611231734!2d-99.14293909999999!3d19.390450899999994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1fefe8b9903ad%3A0x4fe4113243e29a80!2sEstafetas%2099%2C%20Postal%2C%20Benito%20Ju%C3%A1rez%2C%2003410%20Ciudad%20de%20M%C3%A9xico%2C%20CDMX!5e0!3m2!1ses-419!2smx!4v1703724175073!5m2!1ses-419!2smx",
     salsaSchedule: [
-      { days: ["Lunes", "Miércoles"], time: "8:00pm - 9:30pm" },
-      { days: ["Martes", "Jueves"], time: "8:00pm - 9:30pm" },
-      { days: ["Sábados"], time: "10:00am - 12:00pm" },
-      { days: ["Domingos"], time: "4:00pm - 6:00pm" },
+      {
+        days: ["Lunes", "Miércoles"],
+        time: "8:00pm - 9:30pm",
+        enrollUrl:
+          "https://admin.salsa-candela.com/classstripeform?curso=salsa&sucursal=xola&regKey=zG9xKmF3",
+      },
+      {
+        days: ["Martes", "Jueves"],
+        time: "8:00pm - 9:30pm",
+        enrollUrl:
+          "https://admin.salsa-candela.com/classstripeform?curso=salsa&sucursal=xola&regKey=zG9xKmF3",
+      },
+      {
+        days: ["Sábados"],
+        time: "10:00am - 12:00pm",
+        enrollUrl:
+          "https://admin.salsa-candela.com/classstripeform?curso=salsa&sucursal=xola&regKey=zG9xKmF3",
+      },
+      {
+        days: ["Domingos"],
+        time: "4:00pm - 6:00pm",
+        enrollUrl:
+          "https://admin.salsa-candela.com/classstripeform?curso=salsa&sucursal=xola&regKey=zG9xKmF3",
+      },
     ],
     bachataSchedule: [
-      { days: ["Lunes", "Miércoles"], time: "9:30pm - 10:30pm" },
-      { days: ["Martes", "Jueves"], time: "9:30pm - 10:30pm" },
-      { days: ["Sábados"], time: "12:00am - 1:30pm" },
-      { days: ["Domingos"], time: "6:00pm - 7:30pm" },
+      {
+        days: ["Lunes", "Miércoles"],
+        time: "9:30pm - 10:30pm",
+        enrollUrl:
+          "https://admin.salsa-candela.com/classstripeform?curso=bachata&sucursal=xola&regKey=zG9xKmF3",
+      },
+      {
+        days: ["Martes", "Jueves"],
+        time: "9:30pm - 10:30pm",
+        enrollUrl:
+          "https://admin.salsa-candela.com/classstripeform?curso=bachata&sucursal=xola&regKey=zG9xKmF3",
+      },
+      {
+        days: ["Sábados"],
+        time: "12:00am - 1:30pm",
+        enrollUrl:
+          "https://admin.salsa-candela.com/classstripeform?curso=bachata&sucursal=xola&regKey=zG9xKmF3",
+      },
+      {
+        days: ["Domingos"],
+        time: "6:00pm - 7:30pm",
+        enrollUrl:
+          "https://admin.salsa-candela.com/classstripeform?curso=bachata&sucursal=xola&regKey=zG9xKmF3",
+      },
     ],
   },
   {
@@ -31,14 +71,44 @@ const branches = [
     googleMapsEmbedUrl:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3763.9273605566686!2d-99.1738906!3d19.3722977!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ff9a07ea6d93%3A0x984de2ae6b224902!2sF%C3%A9lix%20Cuevas%20407%2C%20Col%20del%20Valle%20Sur%2C%20Benito%20Ju%C3%A1rez%2C%2003104%20Ciudad%20de%20M%C3%A9xico%2C%20CDMX!5e0!3m2!1ses-419!2smx!4v1703724558230!5m2!1ses-419!2smx",
     salsaSchedule: [
-      { days: ["Lunes", "Miércoles"], time: "8:00pm - 9:30pm" },
-      { days: ["Sábados"], time: "11:30am - 1:30pm" },
-      { days: ["Domingos"], time: "11:30pm - 1:30pm" },
+      {
+        days: ["Lunes", "Miércoles"],
+        time: "8:00pm - 9:30pm",
+        enrollUrl:
+          "https://admin.salsa-candela.com/classstripeform?curso=bachata&sucursal=valle&regKey=zG9xKmF3",
+      },
+      {
+        days: ["Sábados"],
+        time: "11:30am - 1:30pm",
+        enrollUrl:
+          "https://admin.salsa-candela.com/classstripeform?curso=salsa&sucursal=valle&regKey=zG9xKmF3",
+      },
+      {
+        days: ["Domingos"],
+        time: "11:30pm - 1:30pm",
+        enrollUrl:
+          "https://admin.salsa-candela.com/classstripeform?curso=salsa&sucursal=valle&regKey=zG9xKmF3",
+      },
     ],
     bachataSchedule: [
-      { days: ["Lunes", "Miércoles"], time: "9:30pm - 10:30pm" },
-      { days: ["Sábados"], time: "1:30pm - 3:00pm" },
-      { days: ["Domingos"], time: "1:30pm - 3:00pm" },
+      {
+        days: ["Lunes", "Miércoles"],
+        time: "9:30pm - 10:30pm",
+        enrollUrl:
+          "https://admin.salsa-candela.com/classstripeform?curso=bachata&sucursal=valle&regKey=zG9xKmF3",
+      },
+      {
+        days: ["Sábados"],
+        time: "1:30pm - 3:00pm",
+        enrollUrl:
+          "https://admin.salsa-candela.com/classstripeform?curso=bachata&sucursal=valle&regKey=zG9xKmF3",
+      },
+      {
+        days: ["Domingos"],
+        time: "1:30pm - 3:00pm",
+        enrollUrl:
+          "https://admin.salsa-candela.com/classstripeform?curso=bachata&sucursal=valle&regKey=zG9xKmF3",
+      },
     ],
   },
   {
@@ -51,12 +121,32 @@ const branches = [
     googleMapsEmbedUrl:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3761.1765622070075!2d-99.2441764!3d19.4910381!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d2031a6b736345%3A0xbce4ed058e944674!2sBlvd.%20de%20la%20Sta.%20Cruz%20142%2C%20Altavista%2C%2003910%20Acatl%C3%A1n%2C%20M%C3%A9x.!5e0!3m2!1ses-419!2smx!4v1703724745453!5m2!1ses-419!2smx",
     salsaSchedule: [
-      { days: ["Lunes", "Miércoles"], time: "8:00pm - 9:30pm" },
-      { days: ["Domingos"], time: "11:30pm - 1:30pm" },
+      {
+        days: ["Lunes", "Miércoles"],
+        time: "8:00pm - 9:30pm",
+        enrollUrl:
+          "https://admin.salsa-candela.com/classstripeform?curso=salsa&sucursal=satelite&regKey=zG9xKmF3",
+      },
+      {
+        days: ["Domingos"],
+        time: "11:30pm - 1:30pm",
+        enrollUrl:
+          "https://admin.salsa-candela.com/classstripeform?curso=salsa&sucursal=satelite&regKey=zG9xKmF3",
+      },
     ],
     bachataSchedule: [
-      { days: ["Lunes", "Miércoles"], time: "9:30pm - 10:30pm" },
-      { days: ["Domingos"], time: "1:30pm - 3:00pm" },
+      {
+        days: ["Lunes", "Miércoles"],
+        time: "9:30pm - 10:30pm",
+        enrollUrl:
+          "https://admin.salsa-candela.com/classstripeform?curso=bachata&sucursal=satelite&regKey=zG9xKmF3",
+      },
+      {
+        days: ["Domingos"],
+        time: "1:30pm - 3:00pm",
+        enrollUrl:
+          "https://admin.salsa-candela.com/classstripeform?curso=bachata&sucursal=satelite&regKey=zG9xKmF3",
+      },
     ],
   },
   {
@@ -68,8 +158,22 @@ const branches = [
     googleMapsUrl: "https://maps.app.goo.gl/4MhAAkVUBsQfdzTo8",
     googleMapsEmbedUrl:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3761.813823212397!2d-99.1824858!3d19.4635922!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1f89b091bdcd5%3A0xfcafed2927bdd9a7!2sPalestina%2010%2C%20Claveria%2C%20Azcapotzalco%2C%2002080%20Ciudad%20de%20M%C3%A9xico%2C%20CDMX!5e0!3m2!1ses-419!2smx!4v1703724856034!5m2!1ses-419!2smx",
-    salsaSchedule: [{ days: ["Martes", "Jueves"], time: "8:00pm - 9:30pm" }],
-    bachataSchedule: [{ days: ["Martes", "Jueves"], time: "9:30pm - 10:30pm" }],
+    salsaSchedule: [
+      {
+        days: ["Martes", "Jueves"],
+        time: "8:00pm - 9:30pm",
+        enrollUrl:
+          "https://admin.salsa-candela.com/classstripeform?curso=salsa&sucursal=claveria&regKey=zG9xKmF3",
+      },
+    ],
+    bachataSchedule: [
+      {
+        days: ["Martes", "Jueves"],
+        time: "9:30pm - 10:30pm",
+        enrollUrl:
+          "https://admin.salsa-candela.com/classstripeform?curso=bachata&sucursal=claveria&regKey=zG9xKmF3",
+      },
+    ],
   },
   {
     id: "coapa",
@@ -80,8 +184,22 @@ const branches = [
     googleMapsUrl: "https://maps.app.goo.gl/RpanSFWu3Efu6PcT7",
     googleMapsEmbedUrl:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3765.822058144884!2d-99.119535!3d19.2901029!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85ce0175b6627251%3A0x72c03df48ec6f443!2sCalz%20Acoxpa%20885%2C%20Coapa%2C%20Coapa%20Super%204%2C%20Tlalpan%2C%2014390%20Ciudad%20de%20M%C3%A9xico%2C%20CDMX!5e0!3m2!1ses-419!2smx!4v1703724927960!5m2!1ses-419!2smx",
-    salsaSchedule: [{ days: ["Martes", "Jueves"], time: "8:00pm - 9:30pm" }],
-    bachataSchedule: [{ days: ["Martes", "Jueves"], time: "9:30pm - 10:30pm" }],
+    salsaSchedule: [
+      {
+        days: ["Martes", "Jueves"],
+        time: "8:00pm - 9:30pm",
+        enrollUrl:
+          "https://admin.salsa-candela.com/classstripeform?curso=salsa&sucursal=coapa&regKey=zG9xKmF3",
+      },
+    ],
+    bachataSchedule: [
+      {
+        days: ["Martes", "Jueves"],
+        time: "9:30pm - 10:30pm",
+        enrollUrl:
+          "https://admin.salsa-candela.com/classstripeform?curso=bachata&sucursal=coapa&regKey=zG9xKmF3",
+      },
+    ],
   },
 ];
 
